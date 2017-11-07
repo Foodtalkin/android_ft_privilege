529a361c374449bc8cd3e661fbbb721f60527f2b
Mandy1125 <mandeep1983.s@gmail.com>
2017-11-07 19:18:22 +0530

    working on experience details

--- app/src/main/java/in/foodtalk/privilege/fragment/experiencesDetails/ExperienceDetailsAdapter.java
+++ app/src/main/java/in/foodtalk/privilege/fragment/experiencesDetails/ExperienceDetailsAdapter.java
@@ -1,18 +1,31 @@
 package in.foodtalk.privilege.fragment.experiencesDetails;
 
 import android.content.Context;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Set;
+
 import in.foodtalk.privilege.R;
+import in.foodtalk.privilege.app.Url;
+import in.foodtalk.privilege.comm.CallbackFragOpen;
 
 /**
  * Created by RetailAdmin on 06-11-2017.
@@ -32,6 +45,8 @@ public class ExperienceDetailsAdapter extends RecyclerView.Adapter<RecyclerView.
     private final int VIEW_VIDEO = 5;
     private final int VIEW_URL = 6;
 
+    CallbackFragOpen callbackFragOpen;
+
     public ExperienceDetailsAdapter (Context context, JSONObject response){
         this.context = context;
         this.response = response;
@@ -41,6 +56,7 @@ public class ExperienceDetailsAdapter extends RecyclerView.Adapter<RecyclerView.
             e.printStackTrace();
         }
         layoutInflater = LayoutInflater.from(context);
+        callbackFragOpen = (CallbackFragOpen) context;
     }
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -87,7 +103,76 @@ public class ExperienceDetailsAdapter extends RecyclerView.Adapter<RecyclerView.
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof CoverCard){
+            CoverCard coverCard = (CoverCard) holder;
+            try {
+                coverCard.tvTitle.setText(response.getJSONObject("result").getString("title"));
+                coverCard.tvAddress.setText(response.getJSONObject("result").getString("address"));
+                //coverCard.tvAddress1.setText(response.getJSONObject("result").getString(""));
 
+                Picasso.with(context)
+                        .load(response.getJSONObject("result").getString("cover_image"))
+                        //.fit()
+                        .placeholder(R.drawable.ic_placeholder)
+                        .fit().centerCrop()
+                        .into(coverCard.imgView);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (holder instanceof DescriptionCard){
+            DescriptionCard descriptionCard = (DescriptionCard) holder;
+            try {
+                descriptionCard.tvTitle.setText(dataList.getJSONObject(position-1).getString("title"));
+                descriptionCard.tvDes.setText(dataList.getJSONObject(position-1).getString("content"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (holder instanceof List1Card){
+            List1Card list1Card = (List1Card) holder;
+            try {
+                list1Card.tvTitle.setText(dataList.getJSONObject(position-1).getString("title"));
+                JSONArray list = dataList.getJSONObject(position-1).getJSONArray("content");
+                String listStr = "- ";
+                for (int i = 0; i < list.length(); i++){
+                    if (list.length() > i+1){
+                        listStr = listStr+list.getString(i)+"\n- ";
+                    }else {
+                        listStr = listStr+list.getString(i);
+                    }
+                }
+                list1Card.tvList.setText(listStr);
+                Log.e("List1Card", listStr);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (holder instanceof UrlCard){
+            UrlCard urlCard = (UrlCard) holder;
+            try {
+                urlCard.tvLink.setText(dataList.getJSONObject(position-1).getString("title"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (holder instanceof VideoCard){
+            VideoCard videoCard = (VideoCard) holder;
+            Uri uri = null;
+            try {
+                uri = Uri.parse(dataList.getJSONObject(position-1).getString("content"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String videoId = uri.getQueryParameter("v");  //will return "V-Maths-Addition "
+
+            videoCard.videoId1 = videoId;
+            Log.e("videoCard","videoId: "+ videoId);
+
+            String summary = "<html><body><iframe width=\"100%\" height=\"200\" src=\"https://www.youtube.com/embed/"+videoId+"?rel=0\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+            videoCard.webView.loadData(summary, "text/html", null);
+        }
     }
     @Override
     public int getItemViewType(int position) {
@@ -145,14 +230,19 @@ public class ExperienceDetailsAdapter extends RecyclerView.Adapter<RecyclerView.
         }
     }
     private class DescriptionCard extends RecyclerView.ViewHolder{
+        TextView tvTitle, tvDes;
         public DescriptionCard(View itemView) {
             super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tv_title);
+            tvDes = (TextView) itemView.findViewById(R.id.tv_des);
         }
     }
     private class List1Card extends RecyclerView.ViewHolder{
-
+        TextView tvTitle, tvList;
         public List1Card(View itemView) {
             super(itemView);
+            tvTitle = (TextView) itemView.findViewById(R.id.tv_title);
+            tvList = (TextView) itemView.findViewById(R.id.tv_list);
         }
     }
     private class List2Card extends RecyclerView.ViewHolder{
@@ -168,15 +258,45 @@ public class ExperienceDetailsAdapter extends RecyclerView.Adapter<RecyclerView.
         }
     }
     private class VideoCard extends RecyclerView.ViewHolder{
-
+        WebView webView;
+        String videoId1;
         public VideoCard(View itemView) {
             super(itemView);
+
+            Log.e("InVideoCard", "videoId1: "+ videoId1);
+
+            //Log.d("html video code", summary);
+            webView = (WebView) itemView.findViewById(R.id.webview);
+            webView.setWebViewClient(new CustomWebViewClient());
+            webView.getSettings().setJavaScriptEnabled(true);
+
+            webView.getSettings().setAppCacheEnabled(false);
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         }
     }
-    private class UrlCard extends RecyclerView.ViewHolder{
-
+    private class UrlCard extends RecyclerView.ViewHolder {
+        TextView tvLink;
         public UrlCard(View itemView) {
             super(itemView);
+            tvLink = (TextView) itemView.findViewById(R.id.tv_link);
+            tvLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        callbackFragOpen.openFrag("webViewFrag",dataList.getJSONObject(getAdapterPosition()-1).getString("content"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
         }
     }
 }
